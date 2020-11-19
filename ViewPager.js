18bcb40d5409c290964b6842a2aef8084b13e29c
anshupurohit1 <74245712+anshupurohit1@users.noreply.github.com>
2020-11-20 02:25:46 +0530

    StaticRenderer path fix for react native web

--- ViewPager.js
+++ ViewPager.js
@@ -15,7 +15,7 @@ var {
   ViewPropTypes,
 } = ReactNative;
 
-var StaticRenderer = require('react-native/Libraries/Components/StaticRenderer');
+var StaticRenderer = require('react-native-web/dist/cjs/vendor/react-native/StaticRenderer/index.js');
 var TimerMixin = require('react-timer-mixin');
 
 var DefaultViewPageIndicator = require('./DefaultViewPageIndicator');
